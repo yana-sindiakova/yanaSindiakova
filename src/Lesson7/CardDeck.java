872769f6a95a373fc5fc10e9115db988efb6128d
Yana Sindiakova <yanasindiakova@gmail.com>
2021-11-22 22:38:40 +0200

    refactor

--- src/Lesson7/CardDeck.java
+++ src/Lesson7/CardDeck.java
@@ -1,19 +1,20 @@
 package Lesson7;
 
-public class CardDeck{
+public class CardDeck {
 
-    public Card[] generateCard (String [] suit, String [] rank, Card [] deck){
-    for (int i = 0; i < rank.length; i++) {
+    public Card[] generateCard(String[] suit, String[] rank, Card[] deck) {
+        for (int i = 0; i < rank.length; i++) {
             for (int j = 0; j < suit.length; j++) {
-                deck [suit.length*i + j] = new Card (rank[i], suit[j]);
+                deck[suit.length * i + j] = new Card(rank[i], suit[j]);
             }
-    } return deck;
+        }
+        return deck;
     }
 
-    public Card[] shuffleDeck (String [] suit, String [] rank, Card [] deck){
+    public Card[] shuffleDeck(String[] suit, String[] rank, Card[] deck) {
         for (int i = 0; i < 100; i++) {
-            int in = (int)((Math.random()*52));
-            int out = (int)((Math.random()*52));
+            int in = (int) ((Math.random() * 52));
+            int out = (int) ((Math.random() * 52));
             Card temp = deck[in];
             deck[in] = deck[out];
             deck[out] = temp;
