da49a7575e22976595938d26a2608a778687c395
Yana Sindiakova <yanasindiakova@gmail.com>
2021-11-29 10:57:39 +0200

    REFACTOR:{Home work - Logger} deleted unused variables

--- src/Lesson9/Logger.java
+++ src/Lesson9/Logger.java
@@ -2,24 +2,10 @@ package Lesson9;
 
 public class Logger {
     private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_BLACK = "\u001B[30m";
     private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_GREEN = "\u001B[32m";
     private static final String ANSI_YELLOW = "\u001B[33m";
     private static final String ANSI_BLUE = "\u001B[34m";
-    private static final String ANSI_PURPLE = "\u001B[35m";
-    private static final String ANSI_CYAN = "\u001B[36m";
-    private static final String ANSI_WHITE = "\u001B[37m";
-
-    private static String message;
-
-    public static String getMessage() {
-        return message;
-    }
-
-    public static void setMessage(String message) {
-        Logger.message = message;
-    }
 
     public static void setLogger(LoggerType type, String message) {
         if (type == LoggerType.ERROR) {
--- src/Lesson9/Main.java
+++ src/Lesson9/Main.java
@@ -2,6 +2,7 @@ package Lesson9;
 
 public class Main {
     public static void main(String[] args) {
+
         Logger.setLogger(LoggerType.ERROR, " No pointer exception");
         Logger.setLogger(LoggerType.DEBUG, " Value=56");
         Logger.setLogger(LoggerType.WARNING, " Library version is deprecated");
