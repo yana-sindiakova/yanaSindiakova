2f94e2ea5775679acaba9d213fc50160bd067baf
Yana Sindiakova <yanasindiakova@gmail.com>
2022-01-14 22:37:24 +0200

    Lesson 22, Homework 14 - Waits

--- src/test/java/HomeWork12Lesson20/ThirdTaskLocation.java
+++ src/test/java/HomeWork12Lesson20/ThirdTaskLocation.java
@@ -6,10 +6,16 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ThirdTaskLocation {
     private WebDriver driver;
@@ -30,22 +36,21 @@ public class ThirdTaskLocation {
     public void getLocation() {
         WebElement locationButton = driver.findElement(By.xpath("//*[text()='Where am I?']"));
         locationButton.click();
-        waitSleep(4000);
 
-        WebElement latitudeValue = driver.findElement(By.xpath("//*[@id='lat-value']"));
-        WebElement longitudeValue = driver.findElement(By.xpath("//*[@id='long-value']"));
+        By latitudeLocator = By.id("lat-value");
+        By longitudeLocator = By.id("long-value");
+
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 5);
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(latitudeLocator));
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(longitudeLocator));
+
+        WebElement latitudeValue = driver.findElement(latitudeLocator);
+        WebElement longitudeValue = driver.findElement(longitudeLocator);
+
 
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(latitudeValue.getText(), "50.4798007");
         softAssert.assertEquals(longitudeValue.getText(), "30.6020642");
-    }
-
-    public void waitSleep(int time) {
-        try {
-            Thread.sleep(time);
-        } catch (InterruptedException | NoAlertPresentException e) {
-            e.printStackTrace();
-        }
     }
 
 }
